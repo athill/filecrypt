b422e8a4fec2eda4bac87896628b7a0b2a09f118
Andy Hill <andy@andyhill.us>
2016-05-06 18:28:29 -0400

    test.py: set up tmp directory

--- test.py
+++ test.py
@@ -1,10 +1,19 @@
-import unittest, os
-from . import FileCrypt
+import unittest, os, uuid, shutil
+from filecrypt.filecrypt import FileCrypt
 from pprint import pprint
 
 class MyTest(unittest.TestCase):
+	__rootdir = os.path.abspath(os.sep)
+
+
+	def setUp(self):
+		self.__tmpdir = os.path.join(self.__rootdir, 'tmp', 'filecrypt-test-'+str(uuid.uuid4())[:8])
+		os.mkdir(self.__tmpdir)
+
+	def tearDown(self):
+		shutil.rmtree(self.__tmpdir)
+
 	def test(self):
-		pprint(dir(__main__))
 		self.assertEqual(4, 4)
 
 
