9d98dee78fe716e05f2e99e3c03327717f96a60c
Andy Hill <andy@andyhill.us>
2016-05-26 18:31:28 -0400

    hoping to publish
    * rename pyfilecrypt.py -> filecrypt.py
    * extract 'main' functionality to cli.py
    * update setup.py

--- __main__.py
+++ __main__.py
@@ -1,48 +1,13 @@
-import argparse, os, re, shutil, sys
-from pprint import pprint
+import os, sys
 
-from pyfilecrypt import (FileCrypt, InvalidPasswordError, InvalidFileTypeError, InvalidFileContentError, 
-    SameInputOutputFileError, InvalidFileError
-)
-
-def main():
-    ## arguments
-    parser = argparse.ArgumentParser(prog='filecrypt', description='Encrypt and decrypt files with password protection')
-    parser.add_argument('action', help='Action to take', choices=set(('encrypt', 'decrypt')))
-    parser.add_argument('file', help='File to encrypt or decrypt. By default will add ".fc" extension when encryting and remove when decrypting')
-    parser.add_argument('-d', '--delete-original', action="store_true", help='Delete original file')
-    parser.add_argument('-o', '--output-file', help='Custom output file')
-    args = parser.parse_args()
-    ## let's do this
-    filecrypt = FileCrypt()
-    ## encrypting
-    if args.action == 'encrypt':
-        password = filecrypt.get_new_password() 
-        try:      
-            filecrypt.encryptfile(args.file, password, args.output_file, args.delete_original)
-            output_file = filecrypt.add_extension(args.file) if args.output_file == None else args.output_file
-            print('* '+args.file+' encrypted as ' + output_file)
-        except (SameInputOutputFileError, InvalidFileError) as e:
-            print('* ' + e.message)
-            exit(1)
-    ## decrypting
-    elif args.action == 'decrypt':
-        password = filecrypt.get_password()
-        try:
-            filecrypt.decryptfile(args.file, password, args.output_file, args.delete_original)
-            output_file = filecrypt.remove_extension(args.file) if args.output_file == None else args.output_file
-            print('* '+args.file+' decrypted as ' + output_file)
-        except (InvalidPasswordError, InvalidFileTypeError, InvalidFileError, InvalidFileContentError, SameInputOutputFileError) as e: 
-            print('* ' + e.message)
-            exit(1)
-
-    if args.delete_original:
-        print('* '+args.file+' deleted')
+from cli import Cli
 
 
 if __name__ == "__main__":
     try:
-        main()
+        # cli = Cli()
+        # cli.main()
+        Cli.main()
     except KeyboardInterrupt:
         print('\nProcess interrupted by user')
         try:
--- setup.py
+++ setup.py
@@ -2,28 +2,36 @@ import ez_setup
 ez_setup.use_setuptools()
 
 import os
-from setuptools import setup, find_packages
+from setuptools import setup # s, find_packages
 # setup(
 #     name = "filecrypt",
 #     version = "0.1",
 #     packages = find_packages(),
 # )
 
+url = "https://github.com/athill/filecrypt"
+requires = ['pycrypto']
 
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
-    name = "pyfilecrypt",
+    name = "athill-filecrypt",
     version = "0.0.4",
     author = "Andy Hill",
     author_email = "andy@andyhill.us",
     description = ("Password protect files."),
+    entry_points = {
+        "console_scripts": ['filecrypt = cli.Cli:main']
+    },    
     license = "BSD",
-    keywords = "example documentation tutorial",
-    url = "https://github.com/athill/filecrypt",
+    keywords = "encode decode encrypt file",
+    url = url,
+    download_url = 'https://github.com/athill/filecrypt/archive/master.zip',
     long_description=read('readme.md'),
-    install_requires = ['pycrypto'],
+
+    install_requires = requires,
+    requires = requires,
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Topic :: Utilities",
